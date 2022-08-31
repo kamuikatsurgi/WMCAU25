c8b462acf6ac4cd504943acac54e7bd15f76f30e
Krishang Shah <109511742+kamuikatsurgi@users.noreply.github.com>
2022-08-31 11:13:43 +0530

    Update todolist.sol

--- todolist.sol
+++ todolist.sol
@@ -2,17 +2,23 @@
 
 pragma solidity ^0.8.0 ; 
 
-contract todolist {
+contract todolist{
     address public manager;
+    uint i=1;
+
     constructor() public{
         manager = msg.sender; 
     }
+
     uint public num_task = 1; 
+
     struct Task{
         string todotask;
         bool status;
     }
+
     mapping (uint => Task) public todo;
+
     modifier onlyOwner(){
         require(
             manager == msg.sender
@@ -21,24 +27,50 @@ contract todolist {
         );
         _;
     }
+
     event taskcreation(
         string todotask,
         bool status
     );
+
     function createTask(string memory _todotask) public onlyOwner{
         todo[num_task] = Task(_todotask, false);
         num_task = num_task + 1;
         emit taskcreation(_todotask, false);
     }
+
     function completeTask(uint _num_task) public onlyOwner{
-        todo[_num_task].status = true;
+            todo[_num_task].status = true;
     }
-    function viewTask(uint _id) public view returns(string memory){
-        return todo[_id].todotask;
+
+    function viewTaskFromitsId(uint _id) public view returns(string memory){
+        if (_id<num_task){
+            return todo[_id].todotask;
+        }else{
+            return "Enter a valid number";
+        }
     }
-/*    function viewTask(uint _id) public view returns(Task memory){
+
+/*  function viewTask(uint _id) public view returns(Task memory){
         return todo[_id];
         For future refrence: the above function will retrun a whole struct but in form of a tuple.
     }*/
-}
 
+    function viewTaskFromitsName(string memory _todo) public returns(uint){
+
+        /*In solidity there is no such thing to compare strings and == operator is not compatible with strings.
+        So if we want to compare two string what we can do is check there hash and if both the strings are equal there hash
+        would also be equal*/
+        //And kessak-256 is ethereum's native hasing algorithm
+
+        while(i<=num_task-1){
+            if(keccak256(abi.encodePacked(_todo)) == keccak256(abi.encodePacked(todo[i].todotask))){
+                return i;
+                break;
+            }
+            else{
+                i=i+1;
+            }
+        }
+    }
+}
